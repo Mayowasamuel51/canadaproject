cedff23d41334ddf0330c2dfa5607923b23683a7
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-06-30 14:03:25 +0100

    new fama

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -4,15 +4,68 @@
 
 @section('main-content')
 
+<div>
+    <div id="header-carousel" class="carousel slide carousel-fade" data-ride="carousel"></div>
+
+    <div class="carousel-inner" role="listb">
+        <!-- .item -->
+        <div class="im activ">
+            <div class="single-slide- slie">
+                <div class="container">
+                    <div class="welcome-hero-content">
+                        <div class="row">
+                            <div class="col-sm-7">
+                                <div class="single-welcome-hero">
+                                    <div class="welcome-hero-txt">
+                                        <h4> Welcome to Our Community!</h4>
+                                        <!-- <h2>cloth covered accent chair</h2> -->
+                                        <p>
+                                            We are more than just a community — we're a movement of learners, creators, and change-makers. Whether you're here to read insightful blog posts, attend exciting Zoom events, sell or buy items in our mini marketplace, or grow through our educational platform — you're in the right place.
+                                        </p>
+                                        <div class="packages-price">
+                                            <p>
+                                                <!-- $ 399.00 -->
+                                                <!-- <del>$ 499.00</del> -->
+                                            </p>
+                                        </div>
+                                        <button class="btn-cart welcome-add-cart" onclick="window.location.href='#'">
+                                            Canada Events
+                                        </button>
+                                        <button class="btn-cart welcome-add-cart welcome-more-info" onclick="window.location.href='#'">
+                                            Become a member
+                                        </button>
+                                    </div><!--/.welcome-hero-txt-->
+                                </div><!--/.single-welcome-hero-->
+                            </div><!--/.col-->
+                            <div class="col-sm-5">
+                                <div class="single-welcome-hero">
+                                    <div class="welcome-hero-img">
+                                        <!-- <img src="assets/images/slider/slider1.png" alt="slider image"> -->
+                                    </div><!--/.welcome-hero-txt-->
+                                </div><!--/.single-welcome-hero-->
+                            </div><!--/.col-->
+                        </div><!--/.row-->
+                    </div><!--/.welcome-hero-content-->
+                </div><!-- /.container-->
+            </div><!-- /.single-slide-item-->
+
+        </div><!-- /.item .active-->
+
+
+    </div>
+</div>
+</div>
 
 <!--populer-products start -->
-		<section id="populer-products" class="populer-products">
-			<div class="container">
-				<div class="populer-products-content">
-                    <div><h2 class="font-semibold">LATEST EVENTS </h2></div>
-					<div class="row">
-						<div class="col-md-3">
-							<!-- <div class="single-populer-products">
+<section id="populer-products" class="populer-products">
+    <div class="container">
+        <div class="populer-products-content">
+            <div>
+                <h2 class="font-semibold">LATEST EVENTS </h2>
+            </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <!-- <div class="single-populer-products">
 								<div class="single-populer-product-img mt40">
 									<img src="assets/images/populer-products/p1.png" alt="populer-products images">
 								</div>
@@ -21,40 +74,40 @@
 									<p>Nemo enim ipsam voluptatem quia volu ptas sit asperna aut odit aut fugit.</p>
 								</div>
 							</div> -->
-						</div>
-						<div class="col-md-6">
-							<div class="single-populer-products">
-								<div class="single-inner-populer-products">
-									<div class="row">
-										<div class="col-md-4 col-sm-12">
-											<div class="single-inner-populer-product-img">
-												<img src="assets/images/populer-products/p2.png" alt="populer-products images">
-											</div>
-										</div>
-										<div class="col-md-8 col-sm-12">
-											<div class="single-inner-populer-product-txt">
-												<h2>
-													<a href="#">
-														latest designed stool <span>and</span> chair
-													</a>
-												</h2>
-												<p>
-													Edi ut perspiciatis unde omnis iste natusina error sit voluptatem accusantium doloret mque laudantium, totam rem aperiam.
-												</p>
-												<div class="populer-products-price">
-													<h4>Sales Start from <span>$99.00</span></h4>
-												</div>
-												<button class="btn-cart welcome-add-cart populer-products-btn" onclick="window.location.href='#'">
-													discover more
-												</button>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-3">
-							<!-- <div class="single-populer-products">
+                </div>
+                <div class="col-md-6">
+                    <div class="single-populer-products">
+                        <div class="single-inner-populer-products">
+                            <div class="row">
+                                <div class="col-md-4 col-sm-12">
+                                    <div class="single-inner-populer-product-img">
+                                        <img src="assets/images/populer-products/p2.png" alt="populer-products images">
+                                    </div>
+                                </div>
+                                <div class="col-md-8 col-sm-12">
+                                    <div class="single-inner-populer-product-txt">
+                                        <h2>
+                                            <a href="#">
+                                                latest designed stool <span>and</span> chair
+                                            </a>
+                                        </h2>
+                                        <p>
+                                            Edi ut perspiciatis unde omnis iste natusina error sit voluptatem accusantium doloret mque laudantium, totam rem aperiam.
+                                        </p>
+                                        <div class="populer-products-price">
+                                            <h4>Sales Start from <span>$99.00</span></h4>
+                                        </div>
+                                        <button class="btn-cart welcome-add-cart populer-products-btn" onclick="window.location.href='#'">
+                                            discover more
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <!-- <div class="single-populer-products">
 								<div class="single-populer-products">
 									<div class="single-populer-product-img">
 										<img src="assets/images/populer-products/p3.png" alt="populer-products images">
@@ -65,12 +118,12 @@
 									</div>
 								</div>
 							</div> -->
-						</div>
-					</div>
-				</div>
-			</div><!--/.container-->
+                </div>
+            </div>
+        </div>
+    </div><!--/.container-->
 
-		</section><!--/.populer-products-->
+</section><!--/.populer-products-->
 
 
 
